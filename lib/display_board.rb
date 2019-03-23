69d91a1961a53c3dc7c7da601d0f75041f2b4b09
Jessica Villarreal <jvillr11@gmail.com>
2019-03-23 18:33:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
-def display_board = "   "
+def display_board [ "   " ]
  puts "Welcome to Tic Tac Toe"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
 end
 
   
